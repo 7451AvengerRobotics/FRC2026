d0987dc19b324e77991e0660a79cf21dd695aa24
Sriram M <srirammartha1@gmail.com>
2026-02-19 16:12:32 -0500

    Intake Updates

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,76 +1,68 @@
 package frc.robot.subsystems.Intake;
 
+import static edu.wpi.first.units.Units.*;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
-import frc.robot.Constants.RobotConstants.IntakeConstants;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants.IntakePivotConstants;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
 
-    private final TalonFX intake = new TalonFX(IntakeConstants.kIntakeID);;
-    private final DigitalInput intakebreak = new DigitalInput(6);
+    private final TalonFX intake = new TalonFX(IntakeConstants.kIntakeID);
+    private final DutyCycleOut motorDutyCycleOut = new DutyCycleOut(0);
     private final NetworkTable intakeTable = NetworkTableInstance.getDefault().getTable("Intake");
 
     public Intake(){
-        super();
+        TalonFXConfiguration cfg = new TalonFXConfiguration()
+        .withMotorOutput(
+            new MotorOutputConfigs()
+                .withInverted(InvertedValue.CounterClockwise_Positive)
+                .withNeutralMode(NeutralModeValue.Coast)
+        ).withFeedback(
+            new FeedbackConfigs()
+                .withRotorToSensorRatio(1)
+                .withSensorToMechanismRatio(IntakePivotConstants.kIntakeGearRatio)
+        ).withCurrentLimits(
+            new CurrentLimitsConfigs()
+                .withStatorCurrentLimit(Amps.of(40))
+                .withStatorCurrentLimitEnable(true)
+        );
 
-        TalonFXConfiguration cfg = new TalonFXConfiguration();
-        cfg.CurrentLimits.StatorCurrentLimitEnable = true;
-        cfg.CurrentLimits.StatorCurrentLimit = 60;
-
-        StatusCode status = StatusCode.StatusCodeNotInitialized;
-        for (int i = 0; i < 5; ++i) {
-            status = intake.getConfigurator().apply(cfg);
-            if (status.isOK()) 
-            break;
-        }
-        if (!status.isOK()) {
-            System.out.println("Could not configure device. Error: " + status.toString());
-        }
-
-        setName("Intake");
+        intake.getConfigurator().apply(cfg);
     }
 
-    public void runIntake(double power){
-        intake.set(power);
-    }
-
-    public boolean getIntakeBreak() {
-        return !intakebreak.get();
-    }
-
-    public boolean switchIntake() {
-        return this.getIntakeBreak() || this.propIntake();
+    public void setIntakePower(double power){
+        intake.setControl(motorDutyCycleOut.withOutput(power));
     }
 
     public boolean propIntake() {
         return Math.abs(intake.getVelocity().getValueAsDouble()) < 2 && (intake.getStatorCurrent().getValueAsDouble() > 50 && intake.getStatorCurrent().getValueAsDouble() < 60) ;
     }
 
-    public Command setintakePower(double power) {
-        return runEnd(
+    public Command runIntake(double power) {
+        return run(
             () -> {
-                this.runIntake(power);
-            }, 
-            () -> {
-                this.runIntake(0);
+                this.setIntakePower(power);
             });
     }
-
 
     @Override
     public void periodic(){
         double velocity = intake.getVelocity().getValueAsDouble();
         double current = intake.getStatorCurrent().getValueAsDouble();
-
-        intakeTable.getEntry("Break").setBoolean(getIntakeBreak());
         intakeTable.getEntry("Stall").setBoolean(propIntake());
         intakeTable.getEntry("Velocity").setDouble(velocity);
         intakeTable.getEntry("Current").setDouble(current);
