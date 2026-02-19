17754169620364ef5fa438220a11a36de07ff4d3
Sriram M <srirammartha1@gmail.com>
2026-02-19 16:12:26 -0500

    Index Updates

--- src/main/java/frc/robot/subsystems/Index.java
+++ src/main/java/frc/robot/subsystems/Index.java
@@ -1,23 +1,59 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Amps;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IndexConstants;
+import frc.robot.Constants.IntakePivotConstants;
 
 public class Index extends SubsystemBase {
 
-    private final TalonFX indexMotor;
+    private final TalonFX indexMotor = new TalonFX(IndexConstants.kIntakeID);
+    private final DutyCycleOut motorDutyCycleOut = new DutyCycleOut(0);
 
     public Index() {
-        indexMotor = new TalonFX(1); 
+        TalonFXConfiguration cfg = new TalonFXConfiguration()
+        .withMotorOutput(
+            new MotorOutputConfigs()
+                .withInverted(InvertedValue.CounterClockwise_Positive)
+                .withNeutralMode(NeutralModeValue.Coast)
+        ).withFeedback(
+            new FeedbackConfigs()
+                .withRotorToSensorRatio(1)
+                .withSensorToMechanismRatio(IntakePivotConstants.kIntakeGearRatio)
+        ).withCurrentLimits(
+            new CurrentLimitsConfigs()
+                .withStatorCurrentLimit(Amps.of(40))
+                .withStatorCurrentLimitEnable(true)
+        ); 
+
+        indexMotor.getConfigurator().apply(cfg);
     }
 
 
     public void run(double speed) {
-        indexMotor.set(speed);
+        indexMotor.setControl(motorDutyCycleOut.withOutput(speed));
     }
 
-   
-    public void stop() {
-        indexMotor.set(0);
+    public Command runIndex() {
+        return run(() -> {
+            this.run(1);
+        });
+    }
+
+    public Command stopIndex() {
+        return run(() -> {
+            this.run(0);
+        });
     }
 }
