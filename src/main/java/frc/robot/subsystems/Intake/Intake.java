a552ef720b4ce3899f20e1f2d4a042547b743746
Sriram M <srirammartha1@gmail.com>
2026-02-22 18:53:21 -0500

    Copilot White Space

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -2,9 +2,6 @@ package frc.robot.subsystems.Intake;
 
 import static edu.wpi.first.units.Units.*;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
@@ -13,58 +10,61 @@ import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
-import frc.robot.Constants.IntakeConstants;
-import frc.robot.Constants.IntakePivotConstants;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants.IntakePivotConstants;
 
 public class Intake extends SubsystemBase {
 
-    private final TalonFX intake = new TalonFX(IntakeConstants.kIntakeID);
-    private final DutyCycleOut motorDutyCycleOut = new DutyCycleOut(0);
-    private final NetworkTable intakeTable = NetworkTableInstance.getDefault().getTable("Intake");
+  private final TalonFX intake = new TalonFX(IntakeConstants.kIntakeID);
+  private final DutyCycleOut motorDutyCycleOut = new DutyCycleOut(0);
+  private final NetworkTable intakeTable = NetworkTableInstance.getDefault().getTable("Intake");
 
-    public Intake(){
-        TalonFXConfiguration cfg = new TalonFXConfiguration()
-        .withMotorOutput(
-            new MotorOutputConfigs()
-                .withInverted(InvertedValue.CounterClockwise_Positive)
-                .withNeutralMode(NeutralModeValue.Coast)
-        ).withFeedback(
-            new FeedbackConfigs()
-                .withRotorToSensorRatio(1)
-                .withSensorToMechanismRatio(IntakePivotConstants.kIntakeGearRatio)
-        ).withCurrentLimits(
-            new CurrentLimitsConfigs()
-                .withStatorCurrentLimit(Amps.of(40))
-                .withStatorCurrentLimitEnable(true)
-        );
+  public Intake() {
+    TalonFXConfiguration cfg =
+        new TalonFXConfiguration()
+            .withMotorOutput(
+                new MotorOutputConfigs()
+                    .withInverted(InvertedValue.CounterClockwise_Positive)
+                    .withNeutralMode(NeutralModeValue.Coast))
+            .withFeedback(
+                new FeedbackConfigs()
+                    .withRotorToSensorRatio(1)
+                    .withSensorToMechanismRatio(IntakePivotConstants.kIntakeGearRatio))
+            .withCurrentLimits(
+                new CurrentLimitsConfigs()
+                    .withStatorCurrentLimit(Amps.of(40))
+                    .withStatorCurrentLimitEnable(true));
 
-        intake.getConfigurator().apply(cfg);
-    }
+    intake.getConfigurator().apply(cfg);
+  }
 
-    public void setIntakePower(double power){
-        intake.setControl(motorDutyCycleOut.withOutput(power));
-    }
+  public void setIntakePower(double power) {
+    intake.setControl(motorDutyCycleOut.withOutput(power));
+  }
 
-    public boolean propIntake() {
-        return Math.abs(intake.getVelocity().getValueAsDouble()) < 2 && (intake.getStatorCurrent().getValueAsDouble() > 50 && intake.getStatorCurrent().getValueAsDouble() < 60) ;
-    }
+  public boolean propIntake() {
+    return Math.abs(intake.getVelocity().getValueAsDouble()) < 2
+        && (intake.getStatorCurrent().getValueAsDouble() > 50
+            && intake.getStatorCurrent().getValueAsDouble() < 60);
+  }
 
-    public Command runIntake(double power) {
-        return run(
-            () -> {
-                this.setIntakePower(power);
-            });
-    }
+  public Command runIntake(double power) {
+    return run(
+        () -> {
+          this.setIntakePower(power);
+        });
+  }
 
-    @Override
-    public void periodic(){
-        double velocity = intake.getVelocity().getValueAsDouble();
-        double current = intake.getStatorCurrent().getValueAsDouble();
-        intakeTable.getEntry("Stall").setBoolean(propIntake());
-        intakeTable.getEntry("Velocity").setDouble(velocity);
-        intakeTable.getEntry("Current").setDouble(current);
-    }
+  @Override
+  public void periodic() {
+    double velocity = intake.getVelocity().getValueAsDouble();
+    double current = intake.getStatorCurrent().getValueAsDouble();
+    intakeTable.getEntry("Stall").setBoolean(propIntake());
+    intakeTable.getEntry("Velocity").setDouble(velocity);
+    intakeTable.getEntry("Current").setDouble(current);
+  }
 }
