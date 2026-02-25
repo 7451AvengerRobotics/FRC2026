78c725dc41537703eaa4a281c2af1aa7f76dbba1
Sriram M <srirammartha1@gmail.com>
2026-02-25 17:42:37 -0500

    shooter updates

--- src/main/java/frc/robot/subsystems/Shooters/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooters/Shooter.java
@@ -1,32 +1,16 @@
 package frc.robot.subsystems.Shooters;
 
-import static edu.wpi.first.units.Units.Amps;
-
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.ctre.phoenix6.configs.FeedbackConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.MotorAlignmentValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkFlexConfig;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
-import com.revrobotics.spark.ClosedLoopSlot;
-import com.revrobotics.spark.FeedbackSensor;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
-
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
+import org.littletonrobotics.junction.Logger;
 
 public class Shooter extends SubsystemBase {
 
@@ -65,50 +49,42 @@ public class Shooter extends SubsystemBase {
     SparkFlexConfig leaderCfg = new SparkFlexConfig();
     SparkFlexConfig followerCfg = new SparkFlexConfig();
 
-    globalCfg.inverted(false)
-              .idleMode(IdleMode.kCoast);
+    globalCfg.idleMode(IdleMode.kCoast);
 
-    globalCfg.smartCurrentLimit(80);
+    globalCfg.smartCurrentLimit(35);
 
-    globalCfg.encoder
-   .positionConversionFactor(1.0 / ShooterConstants.kShooterGearRatio)
-   .velocityConversionFactor(1.0 / ShooterConstants.kShooterGearRatio);
+    globalCfg.closedLoop.p(ShooterConstants.kP);
 
-   /*
-     * Configure the closed loop controller. We want to make sure we set the
-     * feedback sensor as the primary encoder.
-     */
-    globalCfg.closedLoop
-        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-        // Set PID values for velocity control in slot 1
-        .p(ShooterConstants.kP)
-        .feedForward
-          // kV is now in Volts, so we multiply by the nominal voltage (12V)
-          .kV(ShooterConstants.kV)
-          .kS(ShooterConstants.kS);
-
-    globalCfg.closedLoop.maxMotion
+    globalCfg
+        .closedLoop
+        .maxMotion
         // Set MAXMotion parameters for velocity control in slot 1
         .maxAcceleration(500)
-        .cruiseVelocity(6000)
         .allowedProfileError(1);
 
-
     leaderCfg.apply(globalCfg);
-    followerCfg.apply(globalCfg).follow(shooterLeader).inverted(true);
+    // followerCfg.apply(globalCfg).follow(shooterLeader).inverted(true);
 
-    shooterLeader.configure(leaderCfg, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    shooterFollower.configure(followerCfg, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    shooterLeader.configure(
+        leaderCfg, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    shooterFollower.configure(
+        followerCfg, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
-  public void run(double targetRPS) {
-    closedLoopController.setSetpoint(targetRPS, ControlType.kMAXMotionVelocityControl);
+  @Override
+  public void periodic() {
+    Logger.recordOutput("Velocity in RPM", shooterLeader.getEncoder().getVelocity());
+  }
+
+  public void run(double power) {
+    // closedLoopController.setSetpoint(targetRPM, ControlType.kVelocity);
+    shooterLeader.set(power);
   }
 
   public Command runShooter() {
     return run(
         () -> {
-          this.run(1);
+          this.run(0.9);
         });
   }
 
