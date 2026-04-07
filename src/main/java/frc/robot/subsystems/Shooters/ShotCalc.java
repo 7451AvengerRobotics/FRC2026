88a36906019e6a9472f70293a1215fd2049c692a
Aarav Narayan <aarav.narayan@gmail.com>
2026-04-07 12:04:29 -0400

    lerp table values added

--- src/main/java/frc/robot/subsystems/Shooters/ShotCalc.java
+++ src/main/java/frc/robot/subsystems/Shooters/ShotCalc.java
@@ -15,10 +15,59 @@ public class ShotCalc {
   public double pitch = Math.toRadians(60);
   Transform3d turretOffset = new Transform3d();
   InterpolatingDoubleTreeMap angleLerp = new InterpolatingDoubleTreeMap();
+  InterpolatingDoubleTreeMap velocityLerp = new InterpolatingDoubleTreeMap();
 
   public ShotCalc(Transform3d turretOffset) {
     this.turretOffset = turretOffset;
-    angleLerp.put(0.0, 0.0);
+    angleLerp.put(1.0687, 1.315978256);
+    angleLerp.put(1.42,   1.299223095);
+    angleLerp.put(1.87,   1.284911395);
+    angleLerp.put(2.24,   1.265363708);
+    angleLerp.put(2.465,  1.24581602);
+    angleLerp.put(2.7,    1.230457123);
+    angleLerp.put(3.03,   1.211956633);
+    angleLerp.put(3.15,   1.177050048);
+    angleLerp.put(3.3,    1.160993018);
+    angleLerp.put(3.45,   1.145110522);
+    angleLerp.put(3.65,   1.122246709);
+    angleLerp.put(3.84,   1.103920752);
+    angleLerp.put(3.97,   1.086868885);
+    angleLerp.put(4.07,   1.067966969);
+    angleLerp.put(4.15,   1.055575132);
+    angleLerp.put(4.32,   1.037947306);
+    angleLerp.put(4.34,   1.020668547);
+    angleLerp.put(4.41,   0.9848892969);
+    angleLerp.put(4.56,   0.9625490825);
+    angleLerp.put(4.69,   0.9117600012);
+    angleLerp.put(4.63,   0.8873253917);
+    angleLerp.put(4.83,   0.8574802615);
+    angleLerp.put(4.69,   0.8267624667);
+    angleLerp.put(4.94,   0.7766715171);
+
+    velocityLerp.put(1.315978256,  5.628777321);
+    velocityLerp.put(1.299223095,  6.030438178);
+    velocityLerp.put(1.284911395,  6.540398345);
+    velocityLerp.put(1.265363708,  6.859581163);
+    velocityLerp.put(1.24581602,   6.982560199);
+    velocityLerp.put(1.230457123,  7.134449321);
+    velocityLerp.put(1.211956633,  7.350273863);
+    velocityLerp.put(1.177050048,  7.266528096);
+    velocityLerp.put(1.160993018,  7.321765641);
+    velocityLerp.put(1.145110522,  7.378271886);
+    velocityLerp.put(1.122246709,  7.447889085);
+    velocityLerp.put(1.103920752,  7.528828409);
+    velocityLerp.put(1.086868885,  7.569435279);
+    velocityLerp.put(1.067966969,  7.585611046);
+    velocityLerp.put(1.055575132,  7.609483022);
+    velocityLerp.put(1.037947306,  7.685768651);
+    velocityLerp.put(1.020668547,  7.662851550);
+    velocityLerp.put(0.9848892969, 7.654255170);
+    velocityLerp.put(0.9625490825, 7.728963391);
+    velocityLerp.put(0.9117600012, 7.795200170);
+    velocityLerp.put(0.8873253917, 7.772150618);
+    velocityLerp.put(0.8574802615, 7.918865415);
+    velocityLerp.put(0.8267624667, 7.893666440);
+    velocityLerp.put(0.7766715171, 8.159037027);
   }
 
   public double getVelocity(double xf) {
@@ -29,9 +78,10 @@ public class ShotCalc {
   }
 
   public double newGetVelocity(double xf) {
-    double voy = Math.sqrt(2 * g * H);
-    double vx = g * xf / (voy + Math.sqrt(Math.pow(voy, 2) - 2 * g * yf));
-    return Math.sqrt(Math.pow(vx, 2) + Math.pow(voy, 2));
+    // double voy = Math.sqrt(2 * g * H);
+    // double vx = g * xf / (voy + Math.sqrt(Math.pow(voy, 2) - 2 * g * yf));
+    // return Math.sqrt(Math.pow(vx, 2) + Math.pow(voy, 2));
+    return 4000;
   }
 
   public double newGetPitch(double xf) {
@@ -39,7 +89,7 @@ public class ShotCalc {
     // double vx = g * xf / (voy + Math.sqrt(Math.pow(voy, 2) - 2 * g * yf));
     // return Math.atan(voy / vx);
     // return HoodConstants.angleLerp.get(xf);
-    return 5;
+    return angleLerp.get(xf);
   }
 
   public double getVelocity8(ChassisSpeeds vr, double xf) {
